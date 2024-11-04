0a39a01d793705960d7ab864f8d8a0cce10f0a85
bobAGPC <142988042+bobAGPC@users.noreply.github.com>
2024-11-04 07:27:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável em programação?',
+    ' Uma variável é um espaço de armazenamento que pode conter dados e cujo valor pode ser alterado durante a execução do programa.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil?',
+    'A capital do Brasil é Brasília.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Progamação',
+    ' Como se diz "obrigado" em inglês?',
+    ' Em inglês, diz-se "thank you."'
+)
